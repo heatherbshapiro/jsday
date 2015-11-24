ef7e4e22dc55dc70996d7aa4cb74fea30d61fee7
Heather Shapiro <hshapiro@microsoft.com>
2015-11-24 11:51:02 -0500

    environment variables

--- config.js
+++
@@ -1,8 +0,0 @@
-//this file holds configuration variables
-//you can add any that you may need for your application
-
-module.exports = {
-    mobileservices: 'jsday',
-    table: 'urls',
-    appkey: "NkntEYxQWJQhKhCiYKimrExrrTGDGU51"
-}
